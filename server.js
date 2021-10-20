ba16e1d069dc2917d237e486300e0f0bf4405a15
LilyWS <stranger1212ss23@gmail.com>
2021-10-20 13:08:59 -0400

    heroku ready

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://root1:1password1@cluster0.fdiu1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
